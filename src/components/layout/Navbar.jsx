b56bc3b520e43ff45e8de79e0c54c8864a9e0e11
rajca21 <2nikolar1@gmail.com>
2025-03-13 00:47:48 +0100

    navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,7 +1,148 @@
-import React from 'react';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { IoMdMenu, IoMdClose } from 'react-icons/io';
+import { MdLanguage } from 'react-icons/md';
+
+import '../../styles/Navbar.css';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className='h-20 my-auto bg-gray-nav'>
+        {/* Desktop navigation */}
+        <div className='mx-[100px] 2xl:mx-[250px] hidden lg:flex justify-between items-center h-full'>
+          <h1 className='font-montserrat-semibold  text-primary-dark flex flex-col'>
+            <span className='title uppercase'>Galaxy</span>
+            <span className='subtitle'>Apartments</span>
+          </h1>
+
+          <nav className='flex flex-row gap-10'>
+            <NavLink
+              to='/'
+              className={({ isActive }) =>
+                isActive
+                  ? 'nav-active font-montserrat-semibold'
+                  : 'nav-inactive font-montserrat-medium'
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to='/apartments'
+              className={({ isActive }) =>
+                isActive
+                  ? 'nav-active font-montserrat-semibold'
+                  : 'nav-inactive font-montserrat-medium'
+              }
+            >
+              Apartments
+            </NavLink>
+            <NavLink
+              to='/contact'
+              className={({ isActive }) =>
+                isActive
+                  ? 'nav-active font-montserrat-semibold'
+                  : 'nav-inactive font-montserrat-medium'
+              }
+            >
+              Contact
+            </NavLink>
+            <NavLink
+              to='/about-us'
+              className={({ isActive }) =>
+                isActive
+                  ? 'nav-active font-montserrat-semibold'
+                  : 'nav-inactive font-montserrat-medium'
+              }
+            >
+              About us
+            </NavLink>
+          </nav>
+          <button className='rounded-full hover:bg-primary-dark bg-primary text-gray-text btn btn-text font-montserrat-semibold flex items-center justify-center'>
+            Book Now
+          </button>
+        </div>
+
+        {/* Mobile Navigation */}
+        <div className='relative lg:hidden h-full  '>
+          <div className='flex items-center justify-between mx-[20px] sm:mx-[40px] h-full'>
+            <div className='cursor-pointer'>
+              <MdLanguage size={24} />
+            </div>
+            <h1 className='font-montserrat-semibold  text-primary-dark flex flex-col'>
+              <span className='title uppercase'>Galaxy</span>
+              <span className='subtitle'>Apartments</span>
+            </h1>
+            {mobileMenuOpen ? (
+              <IoMdClose
+                className='cursor-pointer text-primary-dark'
+                onClick={() => setMobileMenuOpen(false)}
+                size={26}
+              />
+            ) : (
+              <IoMdMenu
+                className='cursor-pointer text-primary-dark'
+                onClick={() => setMobileMenuOpen(true)}
+                size={26}
+              />
+            )}
+          </div>
+          {mobileMenuOpen && (
+            <div className='absolute w-full bg-gray-smoke z-50 shadow-sm'>
+              <nav className='flex flex-col gap-2 pb-2 justify-center items-center w-full'>
+                <NavLink
+                  to='/'
+                  onClick={() => setMobileMenuOpen(false)}
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'font-montserrat-semibold bg-secondary w-full text-center text-gray-text'
+                      : 'font-montserrat-medium'
+                  }
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to='/apartments'
+                  onClick={() => setMobileMenuOpen(false)}
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'font-montserrat-semibold bg-secondary w-full text-center text-gray-text'
+                      : 'font-montserrat-medium'
+                  }
+                >
+                  Apartments
+                </NavLink>
+                <NavLink
+                  to='/contact'
+                  onClick={() => setMobileMenuOpen(false)}
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'font-montserrat-semibold bg-secondary w-full text-center text-gray-text'
+                      : 'font-montserrat-medium'
+                  }
+                >
+                  Contact
+                </NavLink>
+                <NavLink
+                  to='/about-us'
+                  onClick={() => setMobileMenuOpen(false)}
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'font-montserrat-semibold bg-secondary w-full text-center text-gray-text'
+                      : 'font-montserrat-medium'
+                  }
+                >
+                  About us
+                </NavLink>
+              </nav>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
         primary: {
-          DEFAULT: "#8a728e",
-          dark: "#3f2f41",
-          light: "#b9a5bf",
-          pink: "#d8a7b1",
+          DEFAULT: '#8a728e',
+          dark: '#3f2f41',
+          light: '#b9a5bf',
+          pink: '#d8a7b1',
         },
         secondary: {
-          DEFAULT: "#c49a6c",
-          light: "#e3c5a8"
+          DEFAULT: '#c49a6c',
+          light: '#e3c5a8',
         },
-        ghost: {
-          DEFAULT: "#f4f1f6"
-        }
-      }
+        gray: {
+          smoke: '#f4f1f6',
+          nav: '#f5f5f5',
+          text: '#fefefe',
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};

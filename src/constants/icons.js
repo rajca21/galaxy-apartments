2f4028a84ec0095c3d2fa9b2a1924df552639e34
rajca21 <2nikolar1@gmail.com>
2025-04-30 14:28:26 +0200

    Single Apartment Page

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,5 +1,7 @@
+import ac_room from '../assets/icons/ac_room.svg';
 import airplane from '../assets/icons/airplane.svg';
 import balcony from '../assets/icons/balcony.svg';
+import balcony_room from '../assets/icons/balcony_room.svg';
 import bathtub from '../assets/icons/bathtub.svg';
 import booking from '../assets/icons/booking.svg';
 import booking_contact from '../assets/icons/booking_contact.svg';
@@ -13,6 +15,7 @@ import graph_contact from '../assets/icons/graph_contact.svg';
 import instagram from '../assets/icons/instagram.svg';
 import instagram_contact from '../assets/icons/instagram_contact.svg';
 import instagram_mobile from '../assets/icons/instagram_mobile.svg';
+import kitchen_room from '../assets/icons/kitchen_room.svg';
 import language from '../assets/icons/language.svg';
 import location from '../assets/icons/location.svg';
 import mail from '../assets/icons/mail.svg';
@@ -22,21 +25,28 @@ import museum from '../assets/icons/museum.svg';
 import parking from '../assets/icons/parking.svg';
 import restaurant from '../assets/icons/restaurant.svg';
 import shower from '../assets/icons/shower.svg';
+import shower_room from '../assets/icons/shower_room.svg';
 import shuttle from '../assets/icons/shuttle.svg';
+import soundproof_room from '../assets/icons/soundproof_room.svg';
+import spatub_room from '../assets/icons/spatub_room.svg';
 import star_purple from '../assets/icons/star_purple.svg';
 import star_purple_outline from '../assets/icons/star_purple_outline.svg';
 import storefront from '../assets/icons/storefront.svg';
 import viber from '../assets/icons/viber.svg';
 import viber_contact from '../assets/icons/viber_contact.svg';
 import viber_mobile from '../assets/icons/viber_mobile.svg';
+import view_room from '../assets/icons/view_room.svg';
 import whatsapp from '../assets/icons/whatsapp.svg';
 import whatsapp_contact from '../assets/icons/whatsapp_contact.svg';
 import whatsapp_mobile from '../assets/icons/whatsapp_mobile.svg';
 import wifi from '../assets/icons/wifi.svg';
+import wifi_room from '../assets/icons/wifi_room.svg';
 
 export default {
+  ac_room,
   airplane,
   balcony,
+  balcony_room,
   bathtub,
   booking,
   booking_contact,
@@ -50,6 +60,7 @@ export default {
   instagram,
   instagram_contact,
   instagram_mobile,
+  kitchen_room,
   language,
   location,
   mail,
@@ -59,15 +70,20 @@ export default {
   parking,
   restaurant,
   shower,
+  shower_room,
   shuttle,
+  soundproof_room,
+  spatub_room,
   star_purple,
   star_purple_outline,
   storefront,
   viber,
   viber_contact,
   viber_mobile,
+  view_room,
   whatsapp,
   whatsapp_contact,
   whatsapp_mobile,
   wifi,
+  wifi_room,
 };
--- src/pages/Apartment.jsx
+++ src/pages/Apartment.jsx
@@ -1,11 +1,201 @@
-import images from '../constants/images';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { motion } from 'framer-motion';
+
+import '../styles/ApartmentPage.css';
+import icons from '../constants/icons';
+import { roomsArray } from '../utils/data';
+import PageFirstSectionLayout from '../components/layout/PageFirstSectionLayout';
 
 const Apartment = () => {
+  const [room, setRoom] = useState(null);
+  const { slug } = useParams();
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    if (slug) {
+      const foundRoom = roomsArray.find((room) => room.slug === slug);
+      setRoom(foundRoom || null);
+    }
+  }, [slug]);
+
   return (
-    <div className='h-[calc(100vh-80px)] bg-primary w-full flex flex-col gap-2 items-center justify-center'>
-      <img src={images.build} alt='building' className='size-40' />
-      <p className='font-montserrat-semibold text-gray-text'>In process...</p>
-    </div>
+    <>
+      {room ? (
+        <>
+          <PageFirstSectionLayout bgColor={'bg-gray-nav'}>
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, ease: 'easeOut' }}
+              viewport={{ once: true, amount: 0.2 }}
+              className='bg-white rounded-[25px] p-[25px]'
+            >
+              {/* DEKSTOP */}
+              <div className='hidden lg:flex gap-[25px]'>
+                <div className='flex-1'>
+                  <img src={room?.headerImg} className='rounded-[10px] ' />
+                </div>
+                <div className='flex-1'>
+                  <h1 className='heading font-montserrat-bold text-primary-dark'>
+                    {t(room.titleKey)}
+                  </h1>
+                  <div className='grid grid-cols-3 mt-[25px] gap-[15px]'>
+                    {room.facilities.spatub && (
+                      <div className='flex items-center gap-[9px] h-[35px] max-w-[175px]'>
+                        <img src={icons.spatub_room} alt='spatub_icon' />
+                        <p className='facility_text font-montserrat-normal'>
+                          {t('room_facilities.spatub')}
+                        </p>
+                      </div>
+                    )}
+                    {room.facilities.shower && (
+                      <div className='flex items-center gap-[9px] h-[35px] max-w-[175px]'>
+                        <img src={icons.shower_room} alt='shower_icon' />
+                        <p className='facility_text font-montserrat-normal'>
+                          {t('room_facilities.bathroom')}
+                        </p>
+                      </div>
+                    )}
+                    {room.facilities.wifi && (
+                      <div className='flex items-center gap-[9px] h-[35px] max-w-[175px]'>
+                        <img src={icons.wifi_room} alt='wifi_icon' />
+                        <p className='facility_text font-montserrat-normal'>
+                          {t('room_facilities.wifi')}
+                        </p>
+                      </div>
+                    )}
+                    {room.facilities.balcony && (
+                      <div className='flex items-center gap-[9px] h-[35px] max-w-[175px]'>
+                        <img src={icons.balcony_room} alt='balcony_icon' />
+                        <p className='facility_text font-montserrat-normal'>
+                          {t('room_facilities.balcony')}
+                        </p>
+                      </div>
+                    )}
+                    {room.facilities.ac && (
+                      <div className='flex items-center gap-[9px] h-[35px] max-w-[175px]'>
+                        <img src={icons.ac_room} alt='snow_icon' />
+                        <p className='facility_text font-montserrat-normal'>
+                          {t('room_facilities.ac')}
+                        </p>
+                      </div>
+                    )}
+                    {room.facilities.soundproof && (
+                      <div className='flex items-center gap-[9px] h-[35px] max-w-[175px]'>
+                        <img
+                          src={icons.soundproof_room}
+                          alt='volume_off_icon'
+                        />
+                        <p className='facility_text font-montserrat-normal'>
+                          {t('room_facilities.sound')}
+                        </p>
+                      </div>
+                    )}
+                    {room.facilities.view && (
+                      <div className='flex items-center gap-[9px] h-[35px] max-w-[175px]'>
+                        <img src={icons.view_room} alt='image_icon' />
+                        <p className='facility_text font-montserrat-normal'>
+                          {t('room_facilities.view')}
+                        </p>
+                      </div>
+                    )}
+                    {room.facilities.kitchen && (
+                      <div className='flex items-center gap-[9px] h-[35px] max-w-[175px]'>
+                        <img src={icons.kitchen_room} alt='oven_icon' />
+                        <p className='facility_text font-montserrat-normal'>
+                          {t('room_facilities.kitchen')}
+                        </p>
+                      </div>
+                    )}
+                  </div>
+                  <p className='facility_text mt-[25px]'>
+                    <span className='font-montserrat-semibold'>
+                      {t('room_facilities.size')}:
+                    </span>{' '}
+                    <span className='font-montserrat-normal'>
+                      {room?.size} m<sup>2</sup>
+                    </span>
+                  </p>
+                  <p className='facility_text font-montserrat-normal mt-[25px]'>
+                    {t(room.longDescriptionKey)}
+                  </p>
+                </div>
+              </div>
+
+              {/* MOBILE */}
+              <div className='flex flex-col lg:hidden'>
+                <h1 className='heading font-montserrat-bold text-primary-dark'>
+                  {t(room.titleKey)}
+                </h1>
+                <div className='flex flex-wrap gap-[25px] mt-[25px]'>
+                  {room.facilities.spatub && (
+                    <div className='flex items-center gap-[9px] h-[35px]'>
+                      <img src={icons.spatub_room} alt='spatub_icon' />
+                    </div>
+                  )}
+                  {room.facilities.shower && (
+                    <div className='flex items-center gap-[9px] h-[35px]'>
+                      <img src={icons.shower_room} alt='shower_icon' />
+                    </div>
+                  )}
+                  {room.facilities.wifi && (
+                    <div className='flex items-center gap-[9px] h-[35px]'>
+                      <img src={icons.wifi_room} alt='wifi_icon' />
+                    </div>
+                  )}
+                  {room.facilities.balcony && (
+                    <div className='flex items-center gap-[9px] h-[35px]'>
+                      <img src={icons.balcony_room} alt='balcony_icon' />
+                    </div>
+                  )}
+                  {room.facilities.ac && (
+                    <div className='flex items-center gap-[9px] h-[35px]'>
+                      <img src={icons.ac_room} alt='snow_icon' />
+                    </div>
+                  )}
+                  {room.facilities.soundproof && (
+                    <div className='flex items-center gap-[9px] h-[35px]'>
+                      <img src={icons.soundproof_room} alt='volume_off_icon' />
+                    </div>
+                  )}
+                  {room.facilities.view && (
+                    <div className='flex items-center gap-[9px] h-[35px]'>
+                      <img src={icons.view_room} alt='image_icon' />
+                    </div>
+                  )}
+                  {room.facilities.kitchen && (
+                    <div className='flex items-center gap-[9px] h-[35px]'>
+                      <img src={icons.kitchen_room} alt='oven_icon' />
+                    </div>
+                  )}
+                </div>
+
+                <img
+                  src={room?.headerImg}
+                  className='rounded-[10px] mt-[25px]'
+                />
+
+                <p className='facility_text mt-[25px]'>
+                  <span className='font-montserrat-semibold'>
+                    {t('room_facilities.size')}:
+                  </span>{' '}
+                  <span className='font-montserrat-normal'>
+                    {room?.size} m<sup>2</sup>
+                  </span>
+                </p>
+                <p className='facility_text font-montserrat-normal mt-[25px]'>
+                  {t(room.longDescriptionKey)}
+                </p>
+              </div>
+            </motion.div>
+          </PageFirstSectionLayout>
+        </>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 

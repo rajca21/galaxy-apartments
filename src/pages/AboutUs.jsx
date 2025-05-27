32c39f0553848af23e0a8611114e7a34d4bc7caf
rajca21 <2nikolar1@gmail.com>
2025-05-27 17:05:12 +0200

    Removed react metadata - breaks the code

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,23 +1,8 @@
-import { Metadata } from 'react';
-
 import AboutUsSection from '../components/about_us/AboutUsSection';
 
 const AboutUs = () => {
   return (
     <>
-      <Metadata>
-        <title>Galaxy Apartments</title>
-        <meta name='title' content='Galaxy Apartments' />
-        <meta
-          name='description'
-          content='Find the perfect apartment for your stay in Belgrade.'
-        />
-        <meta property='og:title' content='Galaxy Apartments' />
-        <meta
-          property='og:image'
-          content='https://lh3.googleusercontent.com/gps-cs-s/AC9h4nqpN6hfEGPUcqJfxXrvGjuXyL_WP3NJEHlKGVK1SeRbTJituCbFh2SEVrDInv9IMhVLAR12bG3vGQfMwjB8me1PQXH5fg5g3RpW2-aW-oUgURk_ni-U4f6lWlzfeg-9OfGKSUL2=s680-w680-h510-rw'
-        />
-      </Metadata>
       <AboutUsSection />
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,3 @@
-import { Metadata } from 'react';
-
 import Hero from '../components/home/Hero';
 import Introduction from '../components/home/Introduction';
 import GuestReviews from '../components/home/GuestReviews';
@@ -10,20 +8,6 @@ import ApartmentsSection from '../components/home/ApartmentsSection';
 const Home = () => {
   return (
     <>
-      <Metadata>
-        <title>Galaxy Apartments</title>
-        <meta name='title' content='Galaxy Apartments' />
-        <meta
-          name='description'
-          content='Find the perfect apartment for your stay in Belgrade.'
-        />
-        <meta property='og:title' content='Galaxy Apartments' />
-        <meta
-          property='og:image'
-          content='https://lh3.googleusercontent.com/gps-cs-s/AC9h4nqpN6hfEGPUcqJfxXrvGjuXyL_WP3NJEHlKGVK1SeRbTJituCbFh2SEVrDInv9IMhVLAR12bG3vGQfMwjB8me1PQXH5fg5g3RpW2-aW-oUgURk_ni-U4f6lWlzfeg-9OfGKSUL2=s680-w680-h510-rw'
-        />
-      </Metadata>
-
       <Hero />
       <Introduction />
       <GuestReviews />
